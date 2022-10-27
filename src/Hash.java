68a1030fbcf90d4d2e6a3909a49ccd7b78f49dcd
hellstet <hellstern_tim@web.de>
2022-10-27 12:35:33 +0200

    bugs fixed

---
+++ src/Hash.java
@@ -0,0 +1,23 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+public class Hash {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        MessageDigest md5 = MessageDigest.getInstance("MD5"); //Init
+        String toHash = "was geht ab";
+        md5.update(toHash.getBytes()); //MessageDigest erwartet ein Byte-Array
+        byte[] hash = md5.digest(); //MessageDigest gibt ein Byte-Array zurück
+        String hexValue = byteArrayToHex(hash); //Bytearray in Hexadezimalwert
+        System.out.println(hexValue); //Ausgabe als Hexadezimalwert
+    }
+/**
+ * Wandelt die Eingabe in einen Hexadezimalwert um
+ * @param a das Byte-Array, dass zu einem Hexadezimalwert gewandelt werden soll
+ * @return Hexadezimalrepraesentation von a
+ */
+public static String byteArrayToHex(byte[] a) {
+    StringBuilder sb = new StringBuilder(a.length * 2);
+    for(byte b: a)
+        sb.append(String.format("%02x", b));
+    return sb.toString().toUpperCase();
+}
+}
